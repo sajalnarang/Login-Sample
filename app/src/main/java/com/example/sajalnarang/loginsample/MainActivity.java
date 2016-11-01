3e8e551a6528c3eee92fbec0fad79e199b33959b
Sajal Narang <sajalnarang@gmail.com>
2016-11-02 03:23:29 +0530

    Added references and click listener

--- app/src/main/java/com/example/sajalnarang/loginsample/MainActivity.java
+++ app/src/main/java/com/example/sajalnarang/loginsample/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.sajalnarang.loginsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usernameEditText;
+    EditText passwordEditText;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameEditText = (EditText) findViewById(R.id.username);
+        passwordEditText = (EditText) findViewById(R.id.password);
+        loginButton = (Button) findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                //Send HTTP request
+            }
+        });
     }
 }
